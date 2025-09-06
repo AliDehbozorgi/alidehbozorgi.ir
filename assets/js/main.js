d54bb98714a876cf8792566d42dd3f077e7ecb5a
AliDehbozorgi <alidehbozorgigithub@gmail.com>
2025-09-06 21:45:14 +0330

    my commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+  AOS.init();
+
+
+function openImage(src) {
+  document.getElementById("imageModal").style.display = "flex";
+  document.getElementById("modalImg").src = src;
+}
+
+function closeImage() {
+  document.getElementById("imageModal").style.display = "none";
+}
